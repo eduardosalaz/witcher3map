44937860a28406033a7f06f33f67ca147cbfbd87
Eduardo Salazar <eduardosalaz@outlook.com>
2022-01-19 21:19:59 -0600

    Updating to Hearts of Stone Velen/Novigrad

--- assets/scripts/custom/home.js
+++ assets/scripts/custom/home.js
@@ -6,7 +6,7 @@ window.markers = {};
 //i18n init to translate search results
 $.i18n.init(i18noptions, function() {
 	$.i18n.loadNamespace('v', function() {
-		$.cachedScript("files/scripts/mapdata-velen.js").done(function(script, textStatus) {
+		$.cachedScript("files/scripts/mapdata-hos_velen.js").done(function(script, textStatus) {
 			$.i18n.loadNamespace('s', function() {
 				$.cachedScript("files/scripts/mapdata-skellige.js").done(function(script, textStatus) {
 					$.i18n.loadNamespace('w', function() {
